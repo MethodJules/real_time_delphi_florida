0276e1565684c12cb0e3791f611b050737427b3b
Julien Hofer <julien.hofer@gv.hamburg.de>
2022-05-27 15:30:43 +0200

    Survey Answer Question Form Part II

--- docroot/web/modules/real_time_delphi/src/Form/SurveyAnswerQuestionForm.php
+++ docroot/web/modules/real_time_delphi/src/Form/SurveyAnswerQuestionForm.php
@@ -34,7 +34,7 @@ class SurveyAnswerQuestionForm extends FormBase {
     public function buildForm(array $form, FormStateInterface $form_state, $question_id = 1, $user_pass = '92700a6218a7c745cc1ba13720b5a6') {
         // Get the user answers
         $user_answers = $this->get_answers($question_id, $user_pass);
-        
+
         // check the question type
         $questionType = $this->get_question_type($question_id);
         if (!empty($user_answers) && $questionType != 'group') {
@@ -53,7 +53,7 @@ class SurveyAnswerQuestionForm extends FormBase {
                 $number_of_questions = true;
                 break;
             }
-    
+
             if ($q->type === 'group') {
               $questionGroupIndex++;
             }
@@ -61,12 +61,49 @@ class SurveyAnswerQuestionForm extends FormBase {
         }
 
         // TODO: hier weiterschreiben /survey/survey_answer_question.inc zeile 54
+        if (!is_null($user_pass) && $number_of_questions) {
+            // Load title of the question
+            $text = $questions[$question_index]->title;
+            $questionType = $questions[$question_index]->type;
+
+            if ($questionType === 'group') {
+                $headline = "
+            <h1>Themenbereich " . ($questionGroupIndex + 1) . "</h1>
+            <h2>" . $text . "</h2>";
+            } else {
+                $headline = "
+            <h1>These " . ($question_index - $questionGroupIndex + 1) . "</h1>
+            <h2>" . $text . "</h2>";
+            }
+
+            // Load answers of the question from database
+            $answers = $this->get_answers_from_question($$questions[$question_index]->question_id);
+
+            $query = $this->database->select('question_buttons_title', 'qbt');
+            $query->fields('qbt');
+            $query->condition('question_id', $questions[$question_index]->question_id, '=');
+            $query->orderBy('answer_id');
+            $query->orderBy('button_id');
+            $result = $query->execute();
+
+            // Load button titles
+            $button_title_array =$this->get_button_titles($result);
+
+            $form["question"]['title'] = array(
+                '#type' => 'markup',
+                '#title' => t('survey quesion title'),
+                '#markup' => $headline,
+                '#prefix' => '<div class="question-header">',
+            );
+        }
+
+        return $form;
 
     }
 
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
-        
+
     }
 
     //Diese Funktion holt sich die zuletzt abgegebenen Antworten eines Nutzers zu einer bestimmten Frage aus der DB und gibt
@@ -76,9 +113,9 @@ class SurveyAnswerQuestionForm extends FormBase {
 
     //Antworten eines Nutzers zu einer Frage werden geladen
     $query = $this->database->select('question_user_answers', 'qba');
-    $query->fields('qba', 
-                    ['question_user_answers_id', 'question_id', 
-                        'answer_id', 'answer', 'user_pw', 
+    $query->fields('qba',
+                    ['question_user_answers_id', 'question_id',
+                        'answer_id', 'answer', 'user_pw',
                         'comment', 'is_last_answer']);
     $query->condition('user_pw', $user_passcode, '=');
     $query->condition('question_id', $question_id);
--- docroot/web/modules/real_time_delphi/src/Traits/QuestionTrait.php
+++ docroot/web/modules/real_time_delphi/src/Traits/QuestionTrait.php
@@ -12,25 +12,25 @@ trait QuestionTrait {
    */
   protected function delphi_question_get_answer_sets() {
     $answerSets = array();
-    
-    $sql = "SELECT answer_id, button_id, title 
+
+    $sql = "SELECT answer_id, button_id, title
               FROM {question_buttons_title}
               WHERE question_id IN (SELECT question_id FROM {question})
               ORDER BY answer_id";
-    
+
     $database = \Drupal::database();
     $query = $database->query($sql);
     $result = $query->fetchAll();
-    
+
     // Zusammengehörige Einträge gruppieren
     foreach($result as $item) {
       $answerSets[$item->answer_id][$item->button_id] = $item->title;
     }
-    
+
     // Doppelte Einträge entfernen und Sets nach Anzahl der Einträge sortieren
     $answerSets = array_unique($answerSets, SORT_REGULAR);
     array_multisort(array_map('count', $answerSets), SORT_ASC, $answerSets);
-    
+
     return $answerSets;
   }
 
@@ -51,7 +51,7 @@ trait QuestionTrait {
     $query->condition('question_id', $questionId);
 
     $type = $query->execute();
-    
+
     return $type->fetchField();
   }
 
@@ -95,6 +95,82 @@ trait QuestionTrait {
 
     return $question;
   }
+
+  /**
+   * Get answers from question
+   */
+  protected function get_answers_from_question($question_id) {
+    $database = \Drupal::database();
+    $query = $database->select('question_possible_answers', 'qba');
+    $query->fields('qba');
+    $query->condition('question_id', $question_id);
+    $answer_result = $query->execute();
+
+    foreach ($answer_result as $answer) {
+      $answers[] = $answer;
+    }
+
+    return $answers;
+  }
+
+  /**
+   * Get button titles
+   */
+  protected function get_button_titles($button_title_result) {
+
+    //In diese Variable werden die Antwortmöglichkeiten für jede Frage eingefügt
+    $button_title_array = array();
+
+    //Diese Variable zählt die Durchgänge innerhalb der foreach-Schleife
+    $iterations = 0;
+
+    //Diese Variable zählt die Antwortmöglichkeiten jeder Antwort-ID
+    $response_option_counter = 0;
+
+    //Diese Variable dient zum Vergleich zweier Antwort-IDs
+    $answer_id = 0;
+
+    //Diese Variable zählt die Anzahl der Antworten
+    $answer_id_counter = 0;
+
+    //Es wird über alle Antwortmöglichkeiten iteriert
+    foreach ($button_title_result as $button_title) {
+
+      //Sollte es mindestens der zweite Durchlauf sein:
+      if ($iterations > 0) {
+
+        //Sollte die ID der aktuellen Antwort mit der vorigen übereinstimmen:
+        if ($button_title->answer_id == $answer_id) {
+
+          //Die Antwortmöglichkeit wird um eins erhöht und eingefügt
+          $response_option_counter++;
+          $button_title_array[$answer_id_counter][$response_option_counter] = $button_title->title;
+        } else {
+          //Sollte die ID der aktuellen Antwort nicht mit der vorigen übereinstimmen:
+          $response_option_counter++;
+          $button_title_array[$answer_id_counter][$response_option_counter] = "do not know / no information";
+          $response_option_counter = 0;
+          $answer_id_counter++;
+          $button_title_array[$answer_id_counter][$response_option_counter] = $button_title->title;
+          $answer_id = $button_title->answer_id;
+        }
+      } else {
+
+        //Im ersten Durchlauf wird das Array mit der ersten Antwort-ID und der ersten Antwortmöglichkeit befüllt.
+        $button_title_array[$answer_id_counter][$response_option_counter] = $button_title->title;
+        $answer_id = $button_title->answer_id;
+      }
+      $iterations++;
+    }
+
+    $response_option_counter++;
+    $button_title_array[$answer_id_counter][$response_option_counter] = "do not know / no information";
+
+
+    return $button_title_array;
+  }
+
+
 
 
 }
